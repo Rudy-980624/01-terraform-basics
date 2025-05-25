2ec091ef875d05b53f6cad5566a14f6a7aef6338
Rudy <132281178+Rudy-980624@users.noreply.github.com>
2025-05-26 00:56:41 +0200

    Learning
    Learning and experimenting wit variables

--- 02-more-terraform-basics/main.tf
+++ 02-more-terraform-basics/main.tf
@@ -1,8 +1,19 @@
+variable "iam_user_name_prefix" {
+  type    = string #any: string, bool, list, map, number, set, object, tuple
+  default = "my_iam_user"
+}
+
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_iam_user" "my_iam_users" {
-    count = 2
-    name = "my_iam_user_${count.index}"
+  count = 3
+  name  = "${var.iam_user_name_prefix}_${count.index}"
 }
+/* 
+Take note, count can be changed and TF will apply changes
+without destroying previous configurations.
+This is useful for scaling resources up or down without losing existing configurations.
+*/
+
