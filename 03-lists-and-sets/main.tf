8392603f841eac84bad30ab38c9c72615075f082
Rudy <132281178+Rudy-980624@users.noreply.github.com>
2025-05-26 05:05:30 +0200

    Played with recourse
    Added "for each" to maintain automation of users

--- 03-lists-and-sets/main.tf
+++ 03-lists-and-sets/main.tf
@@ -7,6 +7,10 @@ provider "aws" {
 }
 
 resource "aws_iam_user" "my_iam_users" {
-  count = length(var.names)
-  name  = var.names[count.index]
+  #count = length(var.names)
+  #name  = var.names[count.index]
+  for_each = toset(var.names)   
+    name     = each.value
+    # Convert the list to a set for unique values
+    # Using for_each to create a user for each name in the set
 }
